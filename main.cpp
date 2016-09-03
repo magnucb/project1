7cccc458595a5f6d9888dd80568f42b7fabd5a77
Magnus Chr. Bareid <magnucb@astro.uio.no>
2016-09-03 17:15:04 +0200

    initial file creation
    very modest project initialization

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
+
+
+vector<int> x {N}
+vector<int> u {N}
+
+vector<int> a {N}
+vector<int> b {N-1}
+vector<int> c {N-1}
