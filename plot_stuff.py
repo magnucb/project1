9298152407f99d33fc1073a344ba1bb5d7079790
oyvind <oyvind.svendsen1509@gmail.com>
2016-09-12 16:02:23 +0200

    added some files

--- plot_stuff.py
+++ plot_stuff.py
@@ -3,6 +3,36 @@ import numpy as np
 import os
 import sys
 
+curdir = os.getcwd()
+version = 2
+data_dict = {}
+column_dict = {}
+n_range = [10,100,1000]
+for n in n_range:
+    #loop through different n's
+    with open(curdir+"/data/dderiv_u_python_v%s_n%d.dat"%(version,n), 'r') as infile:
+        full_file = infile.read() #read entire file into text
+        lines = full_file.split('\n') #separate by EOL-characters
+        lines = lines[:-1] #remove last line (empty line)
+        keys = lines.pop(0).split(', ') #use top line as keys for dict.
+        dict_of_content = {}
+        for i,key in zip(range(len(keys)), keys):
+            #loop over keys: h, f2c, f3c
+            dict_of_content[key] = []
+            for j in range(len(lines)):
+                # loop over all lines
+                line = lines[j].split(', ')
+                word = line[i]
+                try:
+                    word = float(word)
+                except ValueError: #word cannot be turned to number
+                    print word
+                    sys.exit("There is something wrong with your data-file \n%s cannot be turned to numbers"%word)
+                dict_of_content[key].append(word)
+        data_dict["n=%d"%n] = dict_of_content
+print data_dict["n=10"]["x"]
+print type(data_dict["n=10"]["x"])
+
 def u_exact(x):
     u =  1.0 - (1.0 - pyl.exp(-10.0))*x - pyl.exp(-10.0*x)
     return u
@@ -33,33 +63,6 @@ def plot_generator(version, n):
     datafile.close()
 
 def harry_plotter():
-    version = 2
-    data_dict = {}
-    column_dict = {}
-    n_range = [10,100,1000]
-    for n in n_range:
-        #loop through different n's
-        with open(curdir+"/data/dderiv_u_python_v%s_n%d.dat"%(version,n), 'r') as infile:
-            full_file = infile.read() #read entire file into text
-            lines = full_file.split('\n') #separate by EOL-characters
-            lines = lines[:-1] #remove last line (empty line)
-            keys = lines.pop(0).split(', ') #use top line as keys for dict.
-            dict_of_content = {}
-            for i,key in zip(range(len(keys)), keys):
-                #loop over keys: h, f2c, f3c
-                dict_of_content[key] = []
-                for j in range(len(lines)):
-                    # loop over all lines
-                    line = lines[j].split(', ')
-                    word = line[i]
-                    try:
-                        word = float(word)
-                    except ValueError: #word cannot be turned to number
-                        print word
-                        sys.exit("There is something wrong with your data-file \n%s cannot be turned to numbers"%word)
-                    dict_of_content[key].append(word)
-            data_dict["n=%d"%n] = dict_of_content
-
     #plot pre-game
     pyl.figure()
     pyl.grid(True)
@@ -77,38 +80,43 @@ def harry_plotter():
         
     u_ex = u_exact(x)
     pyl.plot(x, u_ex, 'k-', label="exact, n=%d"%len(x))
-    pyl.legend(loc="best",prop={"size":8})
+    #pyl.legend(loc="best",prop={"size":8})
     pyl.show()
     return None
 
 def compare_methods(n):
-    None
+    """
+    For a specific length 'n' compare all three methods 
+    with the exact function.
+    """
+    x = pyl.array(data_dict["n=%d"%n]["x"])
+    gen = pyl.array(data_dict["n=%d"%n]["u_gen"])
+    spec = pyl.array(data_dict["n=%d"%n]["u_spec"])
+    LU = pyl.array(data_dict["n=%d"%n]["u_LU"])
+    exact = u_exact(x)
+    pyl.figure("compare methods")
+    pyl.grid(True)
+    pyl.xlabel("x")
+    pyl.ylabel("u(x)")
+    pyl.title("function u for three different methods (n=%d)"%n)
+    pyl.legend(loc='best')
+    pyl.savefig(curdir+"/img/compare_methods_n%d.png"%n)
 def compare_general_n():
     None
 def compare_specific_n():
     None
+def epsilon_plots():
+    None
+    
 
-curdir = os.getcwd()
+
+
+
+
+#curdir = os.getcwd()
 #version = int(sys.argv[1])
 #n       = int(sys.argv[2])
 #plot_generator(version, n)
+pyl.xkcd()
 harry_plotter()
-
-
-with open(localdir+filename, 'r') as infile:
-    full_file = infile.read() #read entire file into text
-    lines = full_file.split('\n') #separate by EOL-characters
-    keys = lines.pop(0).split(', ') #use top line as keys for dict.
-    dict_of_content = {}
-    for i,key in zip(range(len(keys)), keys):
-        #loop over keys: h, f2c, f3c
-        dict_of_content[key] = []
-        for j in range(len(lines)):
-            # loop over all lines
-            line = lines[j].split(', ')
-            word = line[i]
-            try:
-                word = float(word)
-            except ValueError: #word cannot be turned to number
-                sys.exit("There is something wrong with your data-file \ncannot be turned to numbers")
-            dict_of_content[key].append(word)
+pyl.savefig(curdir+"/img/frontpage.png")
