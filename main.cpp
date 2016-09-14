bcdd61bed98f70f64a4c5a5d3114834c679aeebf
Magnus Chr. Bareid <magnucb@astro.uio.no>
2016-09-14 11:28:44 +0200

    working on c++ file
    keep swimming

--- main.cpp
+++ main.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
-#include <vector>
+#include <armadillo>
 
 using namespace std;
+using namespace arma;
 
-int main(int argc, char *argv[])
-{
-    cout << "Hello World!" << endl;
-    return 0;
+void general_tridiag(<vec> &a, <vec> &b, <vec> &c, <vec> &y)
+
+int main(int argc, char *argv[]){
+    cout << "Armadillo version: " << arma_version::as_string() << endl;
+    /* int version = atoi(argv[1]) */;
+    int n = 5;
+
+    double x0 = 0.0;
+    double x1 = 1.0;
+    double h = (x1 - x0)/(n+1.0);
+
+    float a [n] = {-1.0};
+    float b [n+1] = {2.0};
+    float c [n] = {-1.0};
 }
 
-
-vector<int> x {N}
-vector<int> u {N}
-
-vector<int> a {N}
-vector<int> b {N-1}
-vector<int> c {N-1}
-
-vector<int> f {N}
-
-int 4ward(a, b, c){
-
+void general_tridiag(<vec> &a, <vec> &b, <vec> &c, <vec> &y){
+    n = len(y);
+    u = 0;
 }
