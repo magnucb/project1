f6e8f649e27d0319d93fde4bf1375f964f5bbf2f
oyvind <oyvind.svendsen1509@gmail.com>
2016-09-15 17:39:43 +0200

    c++ program finally works

--- main.cpp
+++ main.cpp
@@ -1,22 +1,17 @@
 #include <iostream>
 #include <armadillo>
-#include <math.h> //delete if arma can handle exp
+//#include <math.h> //delete if arma can handle exp
 #include <time.h>
 #include <string>
 
 using namespace std;
 using namespace arma;
 
-int writestring2file (char *arg_filename[],
-		      char *arg_outstring[],
-		      bool arg_delete_file);
-int write3vars2file (char *arg_filename[],
-		     double *arg_a,
-		     double *arg_b,
-		     double *arg_c);
-void general_tridiag(vec &a, vec &b, vec &c, vec &y);
-void specific_tridiag(vec &arg_y);
-void LU_decomp(mat &arg_A);
+void writestring2file(string arg_filename, string arg_outstring);
+int write3vars2file (string arg_filename, double *arg_a, double *arg_b, double *arg_c);
+double general_tridiag(vec &arg_a, vec &arg_b, vec &arg_c, vec &arg_u, vec &arg_y, int n);
+double specific_tridiag(vec &arg_u, vec &arg_y, int n);
+double LU_decomp(mat &arg_A, vec &arg_y);
 
 int main(int argc, char *argv[]){
     cout << "Armadillo version: "
@@ -41,7 +36,10 @@ int main(int argc, char *argv[]){
     }
     else if (argc == 3) {
       n = atoi(argv[1]);
-      LU = atoi(argv[2]);
+      string cmd_arg2(argv[2]);
+      if (cmd_arg2 == "1") {
+          LU = true;
+      };
       cout << "2 cmd-line args: n="
 	   << n << " LU=" << LU
 	   << endl;
@@ -51,9 +49,9 @@ int main(int argc, char *argv[]){
 	   << endl;
     }
     
-    clock_t t_gen, t_spec, t_LU;
+    double t_gen, t_spec, t_LU; //CPU time in seconds it takes to calculate the diff. methods
     
-    //generate x-vector
+    //generate x,f,y -vectors
     double x0 = 0.0;
     double x1 = 1.0;
     double h = (x1 - x0)/(n+1.0);
@@ -62,10 +60,10 @@ int main(int argc, char *argv[]){
     vec f1 = zeros<vec>(n);
     f = 100.0*exp(-10.0*x); //using armadillo
     for (int i=0; i<n; i++){
-      f1[i] = 100.0*exp(-10.0*x[i]);
+      f1(i) = 100.0*exp(-10.0*x(i));
     }  //calculating f elementwise
     vec y = h*h*f;
-    
+
     //generate diagonal vector elements and matrix
     vec a = ones<vec>(n-1); a *= -1.0;
     vec b = ones<vec>(n); b *= 2.0;
@@ -82,37 +80,58 @@ int main(int argc, char *argv[]){
     vec u_LU = y;
 
     //allocate parameters for storing data 
-    string time_filename = "dderiv_time_c++_n" + to_string(n) + "_";
-    string u_filename = "dderiv_u_c++_n" + to_string(n) + "_";
-    if (LU) {
-        time_filename += "LU.dat";
-        u_filename += "LU.dat";
-    }
-    else {
-        time_filename += "tridiag.dat";
-        u_filename += "tridiag.dat";
-    }
-    
+    string time_filename = "/data/dderiv_time_c++.dat";
+    string u_filename = "/data/dderiv_u_c++_n" + to_string(n) + "_";
+
     //start exercises
-    
-    if (not LU) {
-      /*calculate u using the general tridiagonal method*/
-      t_gen = general_tridiag(a, b, c, u_gen, y, n); //turns empty array u_gen into solution
+    if (LU) {
+        /*calculate u using LU-decomposition*/
+        t_LU = LU_decomp(A, u_LU); // turns empty array U_LU into solution
 
-      /*calculate u using the specific tridiagonal method*/
-      t_spec = specific_tridiag(u_spec, u_spec, y, n); //turns empty array u_spec into solution
+        //write timing results to file
+        string string_time_data = "method: n=" + to_string(n) + " time=";
+        writestring2file(time_filename, "LU" + string_time_data + to_string(t_LU));
+        //write u(x)-results to file
+        u_filename += "LU.dat";
+        //remove old files
+        //ofstream::outfile.open(u_filename)
+        string string_u_data = "x, u_LU";
+        writestring2file(u_filename, string_u_data);
+        for (int i=0; i < n; i++) {
+            string_u_data = to_string(x(i)) + ", " + to_string(u_gen(i)) + ", " + to_string(u_spec(i));
+            writestring2file(u_filename, string_u_data);
+        }
+    } else {
+        /*calculate u using the general tridiagonal method*/
+        t_gen = general_tridiag(a, b, c, u_gen, y, n); //turns empty array u_gen into solution
 
-      //write timing-results to file
-      //write u(x) to file
+        /*calculate u using the specific tridiagonal method*/
+        t_spec = specific_tridiag(u_spec, y, n); //turns empty array u_spec into solution
+
+        //TEST
+        cout << "TEST:  calculated specific_tridiag" << endl;
+
+        //write timing-results to file
+        string string_time_data = "tridiagonal method: n=" + to_string(n) + " time=";
+        writestring2file(time_filename, "general" + string_time_data + to_string(t_gen));
+        writestring2file(time_filename, "specific" + string_time_data + to_string(t_spec));
+
+        //TEST
+        cout << "TEST:  wrote time to datafile" << endl;
+
+        //write u(x)-results to file
+        u_filename += "tridiag.dat";
+        string string_u_data = "x, u_gen, u_spec";
+        ofstream outfile; outfile.open(u_filename.c_str()); outfile.close();
+
+        writestring2file(u_filename, string_u_data);
+        for (int i=0; i < n; i++) {
+            string_u_data = to_string(x(i)) + ", " + to_string(u_gen(i)) + ", " + to_string(u_spec(i));
+            writestring2file(u_filename, string_u_data);
+        }
+        //TEST
+        cout << "TEST:  wrote u to data file" << endl;
     }
-    else {
-      /*calculate u using LU-decomposition*/
-      t_LU = LU_decomp(A, u_LU); // turns empty array U_LU into solution
-
-      //write timing results to file
-      //write u(x) to file
-    }
-
 }
 
 double general_tridiag(vec &arg_a, vec &arg_b, vec &arg_c, vec &arg_u, vec &arg_y, int n){
@@ -120,29 +139,47 @@ double general_tridiag(vec &arg_a, vec &arg_b, vec &arg_c, vec &arg_u, vec &arg_
     clock_t t0, t1;
 
     for (int i=0; i<n-1; i++){
-        k = arg_a[i]/arg_b[i];
-        arg_b[i+1] -= k*arg_c[i];
-        arg_y[i+1] -= k*arg_y[i];
+        k = arg_a(i)/( (double) arg_b(i) );
+        arg_b(i+1) -= k*arg_c(i);
+        arg_y(i+1) -= k*arg_y(i);
     }
-    for (int i=n-1; i>=0; i--){
-        arg_u[i] = (arg_y[i] - arg_u[i+1]*arg_c[i])/arg_b[i];
+    for (int i=n-2; i>=0; i--){
+        arg_u(i) = (arg_y(i) - arg_u(i+1)*arg_c(i))/( (double) arg_b(i) );
     }
-    return (t1 - t0)/CLOCKS_PER_SECOND
+    return (t1 - t0)/((double) CLOCKS_PER_SEC);
 }
+
 double specific_tridiag(vec &arg_u, vec &arg_y, int n){
+    //TEST
+    cout << "TEST:  started specific tridiag" << endl;
+
     clock_t t0,t1;
-    vec d = zeros<vec>(n-2);
+    vec d = zeros<vec>(n);
+
+    //TEST
+    cout << "TEST:  started forward subst." << endl;
 
     for (int i=1; i<=n-1; i++){
-        d[i-1] = (i+1)/i;
+        d(i-1) = (i+1)/( (double) i );
+        arg_y(i) -= arg_y(i-1)/d(i-1);
+        cout << i << endl
+             << d(i-1) << endl
+             << arg_y(i) << endl;
     }
-    for (int i=1; i<=n-1; i++){
-        arg_y[i] -= arg_y[i-1]/d[i-1];
+
+    //TEST
+    cout << "TEST:  ended forward subst." << endl;
+    //TEST
+    cout << "TEST:  started backward subst." << endl;
+
+    for (int i=n-2; i>=1; i--){
+        arg_u(i) = (arg_y(i) + arg_u(i+1))/d(i);
     }
-    for (int i=n-1; i>=1; i--){
-        arg_u[i] = (arg_y[i] + arg_u[i+1])/d[i];
-    }
-    return (t1 - t0)/CLOCKS_PER_SECOND
+
+    //TEST
+    cout << "TEST:  ended backward subst." << endl;
+
+    return (t1 - t0)/((double) CLOCKS_PER_SEC);
 }
 
 double LU_decomp(mat &arg_A, vec &arg_y){
@@ -157,41 +194,29 @@ double LU_decomp(mat &arg_A, vec &arg_y){
     solve(U, arg_y); //solve U*u = w (where u is stored in the w-array(which is stored in the y-array))
     //array of argument 'arg_y' has now become the solution u of 'A*u = y'
 
-    return (t1 - t0)/CLOCKS_PER_SECOND
+    return (t1 - t0)/((double) CLOCKS_PER_SEC);
 }
 
-int writestring2file (char *arg_filename[], char *arg_outstring[], bool arg_delete_file) {
+void writestring2file (string arg_filename, string arg_outstring) {
   /* Take one single string and add it to the 
      last line of [filename] (defined locally),
      then add endline and close file.
      This is slow and unefficient, but not more is needed.
   */
-  if (arg_delete_file) {
-    /*delete preexisting file and exit*/
-    if( std::remove( arg_filename ) == 0 ) {
-      std::cout << "one file successfully deleted:" << std::endl
-        << "\t" << arg_filename << std::endl;
-      return 0;
-    } //if removed succesfully
-    else {
-      std::cout << "could not delete file:" << std::endl
-        << "\t" << arg_filename << std::endl;
-      return 0;
-    } //else not removed succesfully
-  } //if boolean 'delete_file'
   
-  std::ofstream outfile;
-  outfile.open(arg_filename, std::ios::app);
-  outfile << arg_outstring << std::endl;
+  ofstream outfile;
+  outfile.open(arg_filename, ios::app);
+  outfile << arg_outstring << endl;
   outfile.close();
-  std::cout << "wrote string to: " << std::endl
-        << "\t" << arg_filename << std::endl;
-  return 0;
+  cout << "wrote string to: " << endl
+        << "\t" << arg_filename << endl;
 } // writestring2file
 
-int write3vars2file (char *arg_filename[], double *arg_a, double *arg_b, double *arg_c) {
+
+/*
+void write3vars2file (string arg_filename, double *arg_a, double *arg_b, double *arg_c) {
   /* Open a file and append three variables to it,
-     using the csv-format.*/
+     using the csv-format.//
   std::ofstream outfile;
   outfile.open(arg_filename, std::ios::app);
   outfile << std::scientific << std::setprecision(20)
@@ -199,7 +224,5 @@ int write3vars2file (char *arg_filename[], double *arg_a, double *arg_b, double 
       << arg_b << ", "
       << arg_c << std::endl;
   outfile.close();
-  /*std::cout << "writing skalars to file: " << std::endl
-    << "\t" << filename << std::endl;*/
-  return 0;
-} // write3vars2file
+    } // write3vars2file
+*/
